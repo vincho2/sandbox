0a21b8e44a1d658536711f701cae8f6e5879190a
Vincent Montmoreau <vmontmoreau@murex.com>
2025-10-04 17:22:46 +0200

    auto commit 2025-10-04 17:22:46

--- python/test.py
+++ python/test.py
@@ -1,4 +1,23 @@
-people = [
-    {"name": "Vincent", "number":"1235"},
-    {"name": "Claire", "number":"2359"}
-]
+# Initialize coin list in cents and change amount
+coins = [25, 10, 5, 1]
+change = 0
+
+# Ask the user how much change is owed until he/she provides a positive float
+while True:
+    change = float(input("Change: ")) * 100
+    if change > 0:
+        break
+
+# Initialise coin count
+coin_count = 0
+
+# Compute the number of coins to be given back
+for coin in coins:
+    if change == 0:
+        break
+    elif change >= coin:
+        coin_count = change // coin
+        change = change % coin
+
+# Diplay result
+print(int(coin_count))
